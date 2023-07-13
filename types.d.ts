ad547169abb9049214287deb1002a055a4dd98b9
ghislaingenay <ghislain.gny@gmail.com>
2023-07-13 16:11:29 +0700

    Create types for Post (might change again)

---
+++ types.d.ts
@@ -0,0 +1,28 @@
+type PostTopic =
+  | "DATA SCIENCE"
+  | "WEB DEVELOPMENT"
+  | "MACHINE LEARNING"
+  | "DEEP LEARNING"
+  | "DEVOPS"
+  | "DATABASES"
+  | "BIG DATA"
+  | "OTHERS";
+
+enum Language {
+  ENGLISH = "ENGLISH",
+  FRENCH = "FRENCH",
+}
+
+interface Post {
+  postId: string;
+  title: string;
+  topic: PostTopic[];
+  subTopic: string[];
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+  readTime: string;
+  keywords: string[];
+  series: string;
+  language: Language;
+}
