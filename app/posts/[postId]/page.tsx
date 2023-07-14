e8b78694e7045f893a0b08dc329e5492f2d81ed3
ghislaingenay <ghislain.gny@gmail.com>
2023-07-14 14:41:23 +0700

    Add initial data + not found in postId

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,13 +1,29 @@
+import { getPostById } from "@/lib/api/post-api";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import PostItem from "./PostItem";
+
 interface PostProps {
   params: {
     postId: string;
   };
 }
 
-export default function Post({ params: { postId } }: PostProps) {
-  return (
-    <div>
-      <h1>Post</h1>
-    </div>
-  );
+// inside fetch('', {next: {revalidate: 60}})
+
+export async function generateMetadata({
+  params: { postId },
+}: PostProps): Promise<Metadata> {
+  const post = await getPostById(postId);
+  if (!post) {
+    return { title: "Post Not Found" };
+  }
+  return { title: "Post", description: "post.title", keywords: "post.title" };
+}
+
+export default async function Post({ params: { postId } }: PostProps) {
+  const postData = await await getPostById(postId);
+
+  if (!postData) notFound();
+  return <PostItem />;
 }
