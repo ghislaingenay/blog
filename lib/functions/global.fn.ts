961edebc3ead0f7dfd33207615916b1e763bfa99
ghislaingenay <ghislain.gny@gmail.com>
2023-07-17 11:59:37 +0700

    Create function to get formatted tag text

--- lib/functions/global.fn.ts
+++ lib/functions/global.fn.ts
@@ -7,3 +7,14 @@ export const createMetaData = (meta: Prettify<MissingMetadata>): Metadata => {
 
 export const capitalize = (str: string): string =>
   str ? str.charAt(0).toUpperCase() + str.slice(1) : "";
+
+export const parseJS = (str: string): string => {
+  const containJS = /(.*)js/g.test(str);
+  if (!containJS) return str;
+  return `${str.replace(/js$/g, "")} js`;
+};
+
+export const parseTag = (tag: string): string => {
+  const words = tag.split("-");
+  return words.map((word) => parseJS(word).toUpperCase()).join(" ");
+};
