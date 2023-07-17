f60fe187b9fac36dece6ae265033031714cbd2f5
ghislaingenay <ghislain.gny@gmail.com>
2023-07-17 17:51:38 +0700

    Reorganize tags page

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -9,12 +9,13 @@ export interface TagProps {
 }
 
 export const revalidate = REVALIDATION_PERIOD;
+export const dynamic = "force-dynamic";
 
 export async function generateStaticParams() {
   const posts = await getPostsMeta(); //deduped!
   if (!posts) return [];
-  const keywords = new Set(posts.map((post) => post.keywords).flat());
-  return Array.from(keywords).map((keyword) => ({ tag: keyword }));
+  const tags = new Set(posts.map((post) => post.tags).flat());
+  return Array.from(tags).map((tag) => ({ tag }));
 }
 
 export function generateMetadata({ params: { tag } }: TagProps) {
@@ -26,12 +27,12 @@ export default async function TagList({ params: { tag } }: TagProps) {
   if (!posts)
     return <p className="mt-5 text-center">Sorry, no posts available.</p>;
 
-  const tagPosts = posts.filter((post) => post.keywords.includes(tag));
+  const tagPosts = posts.filter((post) => post.tags.includes(tag));
 
   const tagHavePosts = tagPosts.length > 0;
 
   return (
-    <>
+    <div className="mt-10">
       {tagHavePosts ? (
         <>
           <div className="flex flex-col gap-4">
@@ -48,6 +49,6 @@ export default async function TagList({ params: { tag } }: TagProps) {
           </p>
         </>
       )}
-    </>
+    </div>
   );
 }
