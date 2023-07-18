d62de53741bcde086671540588ffbef8f0988be0
ghislaingenay <ghislain.gny@gmail.com>
2023-07-18 15:50:36 +0700

    Re add section (responsive for main page)

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,9 @@ export default async function Home() {
     return <p className="mt-10 text-center">Sorry, no posts available.</p>;
   }
   return (
-    <section className="my-10 mx-auto">
-      <ul className="list-none p-0">
+    <section className="my-10 mx-auto grid grid-cols-5 lg:grid-cols-4 gap-5">
+      <div className="col-span-5 lg:col-span-1 border-black border h-[100px]" />
+      <ul className="col-span-5 list-none p-0 lg:col-span-3">
         {posts.map((post) => (
           <PostItem key={post.id} post={post} />
         ))}
