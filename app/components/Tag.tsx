4d1dc60d206de59cd70897331d39fa57863c153e
ghislaingenay <ghislain.gny@gmail.com>
2023-07-18 10:21:22 +0700

    Create client tag component

---
+++ app/components/Tag.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { parseTag } from "@functions";
+import { ReactNode } from "react";
+
+type ButtonProps = React.ComponentProps<"button">;
+type TagColor = "blue" | "red" | "green" | "orange" | "gray";
+
+interface TagProps {
+  children: ReactNode;
+  className?: ButtonProps["className"];
+  color?: TagColor;
+  onTagClicked?: () => void;
+}
+
+export const Tag = ({
+  children,
+  className,
+  onTagClicked,
+  color = "blue",
+}: TagProps) => {
+  const colorList: Record<TagColor, string> = {
+    blue: `bg-blue-600`,
+    red: `bg-red-600`,
+    green: `bg-green-600`,
+    orange: `bg-orange-600`,
+    gray: `bg-gray-600`,
+  };
+
+  const isClickable = onTagClicked ? true : false;
+
+  const initialClasses =
+    "text-white w-auto px-3 py-1 text-xs font-medium text-bold rounded-full mx-1";
+  const classes = `${initialClasses} ${className} ${colorList[color]}`;
+  return (
+    <button
+      className={classes}
+      onClick={() => onTagClicked && onTagClicked()}
+      disabled={!isClickable}
+    >
+      {parseTag(children as string)}
+    </button>
+  );
+};
