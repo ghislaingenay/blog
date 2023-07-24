fa202ca2bd05514a467c468ef801d6c4d8b1da0e
ghislaingenay <ghislain.gny@gmail.com>
2023-07-24 12:29:38 +0700

    Able to filter system using jquery

--- app/components/posts/TopicList.tsx
+++ app/components/posts/TopicList.tsx
@@ -2,7 +2,7 @@
 
 import { DivProps, PostTopic } from "@interfaces/global.interface";
 import $ from "jquery";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { BiCode } from "react-icons/bi";
 import { Tag } from "../Tag";
 
@@ -17,44 +17,46 @@ interface TopicDivProps extends DivProps {
 }
 
 export const TopicList = ({ topics }: TopicListProps) => {
-  const removeSpaceFromTopicName = (name: string) => name.replaceAll(" ", "_");
   const addSpaceToTopicName = (name: string) => name.replaceAll("_", " ");
+  const addNameStrToTopicName = (topicName: string) => `name-${topicName}`;
+  const removeNameStrToTopicName = (topicName: string) =>
+    topicName?.replace("name-", "") || topicName;
+
   const topicList = topics.map((topic) => {
-    return { ...topic, name: removeSpaceFromTopicName(topic.name) };
+    return { ...topic, id: addNameStrToTopicName(topic.name) };
   });
+  console.log("tp", topics);
 
-  const ACTIVE_DIV_CLASS = "bg-slate-300 rounded-lg bg-opacity-0.5";
+  const ACTIVE_LI_CLASS = "bg-slate-300 rounded-lg bg-opacity-0.5";
 
-  const isSelected = (name: string) => name === selectedTopic;
-  const generateId = (topicName: string) => `name-${topicName}`;
   const [selectedTopic, setSelectedTopic] = useState<string | undefined>(
     undefined
   );
+  const isSelected = (name: string) => name === selectedTopic;
+  // const isSelected = (name: string) => $(`li#${name}`).hasClass("selected");
+  const [generatedId, setGeneratedId] = useState<string | undefined>(undefined);
 
   // const idLink = idHTMLLiElement.replace("name-", "");
-  // console.log(idLink);
-  const changeDisplayData = (
-    isSelected: boolean,
-    idHTMLLiElement: string
-  ) => {};
 
-  $("li")
-    .off("click")
-    .on("click", function (e) {
-      e.stopPropagation();
-      const liElement = ($(this).parent("id") as any)
-        .prevObject[0] as HTMLLIElement;
-      const idHTMLLiElement = liElement.getAttribute("id") as string;
-      const isSelectedElement = isSelected(idHTMLLiElement);
-      if (isSelectedElement) setSelectedTopic(undefined);
-      else setSelectedTopic(idHTMLLiElement);
-      changeDisplayData(isSelectedElement, idHTMLLiElement);
-    });
+  useEffect(() => {
+    $("li")
+      .off("click")
+      .on("click", function (e) {
+        const liElement = ($(this).parent("id") as any)
+          .prevObject[0] as HTMLLIElement;
+        const idHTMLLiElement = liElement.getAttribute("id") as string;
+        setGeneratedId(
+          (Math.random() * 10000).toString(36).substring(2) +
+            "+" +
+            idHTMLLiElement
+        );
+      });
+  }, []);
 
   const LI_CLASS_LG =
     "grid px-5 grid-cols-6 place-self-center justify-between items-center mb-1 py-2";
 
-  const topicIcons: Record<PostTopic, any> | any = {
+  const topicIconsLg: Record<PostTopic, any> | any = {
     [PostTopic.DATABASES]: "FaDatabase",
     [PostTopic.DATA_SCIENCE]: "FaDatabase",
     [PostTopic.AI]: "FaDatabase",
@@ -64,10 +66,33 @@ export const TopicList = ({ topics }: TopicListProps) => {
     [PostTopic.DEVOPS]: "FaDatabase",
   };
 
+  const topicKeys = Object.keys(PostTopic);
+
+  useEffect(() => {
+    const listId = generatedId?.split("+")[1];
+    const topicKey = removeNameStrToTopicName(listId as string);
+    console.log("listId", listId, topicKey);
+    console.log("hhh", $(`a#WEB_DEVELOPMENT`).get());
+    if (listId === selectedTopic) {
+      topicKeys.forEach((topic) => {
+        $(`a#${topic}`).removeClass("hidden");
+      });
+      return setSelectedTopic(undefined);
+    } else {
+      const unSelectedTopicKeys = topicKeys.filter((key) => key !== topicKey);
+      unSelectedTopicKeys.forEach((topic) => {
+        $(`a#${topic}`).addClass("hidden");
+      });
+      $(`a#${topicKey}`).removeClass("hidden");
+      return setSelectedTopic(listId);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [generatedId]);
+
   const setActivatedStyle = useCallback(
-    (topic: string) => {
-      if (selectedTopic === topic) return `${LI_CLASS_LG} ${ACTIVE_DIV_CLASS}`;
-      else return LI_CLASS_LG;
+    (name: string) => {
+      if (name === selectedTopic) return `${LI_CLASS_LG} ${ACTIVE_LI_CLASS}`;
+      else return `${LI_CLASS_LG}`;
     },
     [selectedTopic]
   );
@@ -76,14 +101,14 @@ export const TopicList = ({ topics }: TopicListProps) => {
     <>
       {/* ----* Small screen *---- */}
       <div className="grid lg:hidden grid-cols-3 sm:grid-cols-4 md:grid-cols-6">
-        {topicList.map(({ name }) => (
+        {topics.map(({ name }) => (
           <div
             key={name}
             id={`name-${name}`}
             className="flex flex-wrap justify-between items-center"
           >
-            {topicIcons[name] && (
-              <span className="col-span-1">{topicIcons[name]}</span>
+            {topicIconsLg[name] && (
+              <span className="col-span-1">{topicIconsLg[name]}</span>
             )}
           </div>
         ))}
@@ -95,18 +120,30 @@ export const TopicList = ({ topics }: TopicListProps) => {
           Main topics
         </h3>
         <div className="list-none block" role="list">
-          {topicList.map(({ name, count }) => (
-            <li
-              className={setActivatedStyle(generateId(name))}
-              key={name}
-              id={generateId(name)}
-            >
-              <span className="col-span-5 text-sm font-bold m-0">{name} </span>
-              <small className="col-span-1">
-                <Tag color="gray">{count}</Tag>
-              </small>
-            </li>
-          ))}
+          {topicList.map(({ name, count, id }) => {
+            return (
+              <>
+                <li className={setActivatedStyle(id)} key={name} id={id}>
+                  <span className="col-span-5 text-sm font-bold m-0">
+                    {addSpaceToTopicName(name)}{" "}
+                  </span>
+                  <small className="col-span-1">
+                    <Tag color="gray">{count}</Tag>
+                  </small>
+                </li>
+                <li
+                  className={setActivatedStyle("name-dfdg")}
+                  key={name}
+                  id="name-dfdg"
+                >
+                  <span className="col-span-5 text-sm font-bold m-0">Gt </span>
+                  <small className="col-span-1">
+                    <Tag color="gray">3</Tag>
+                  </small>
+                </li>
+              </>
+            );
+          })}
         </div>
       </div>
     </>
