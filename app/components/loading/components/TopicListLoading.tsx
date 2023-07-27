7716feafeaa660f726f5a7b13dc68068ace0acdc
ghislaingenay <ghislain.gny@gmail.com>
2023-07-27 13:42:06 +0700

    loading:finish root page

--- app/components/loading/components/TopicListLoading.tsx
+++ app/components/loading/components/TopicListLoading.tsx
@@ -1,22 +1,20 @@
 export const TopicListLoading = () => (
   <div
     role="status"
-    className="max-w-md p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700"
+    className="p-2 sm:p-4 lg:space-y-4 border border-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700"
   >
-    <div className="hiden lg:flex items-center justify-between">
-      <div>
-        <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+    {[1, 2, 3, 4].map((_, index) => (
+      <div className="hidden lg:flex justify-between" key={index}>
+        <div className="h-2.5 flex bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+        <div className="h-2.5 flex bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
       </div>
-      <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-    </div>
-    <div className="grid grid-cols-4 lg:hidden gap-3">
+    ))}
+
+    <div className="grid grid-cols-4 lg:hidden gap-x-3">
       {[1, 2, 3, 4].map((_, index) => (
-        <div
-          key={index}
-          className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 col-span-1"
-        >
-          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
-          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+        <div key={index} className="col-span-1">
+          <div className="h-12 bg-gray-300 rounded-full dark:bg-gray-700 "></div>
+          {/* <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div> */}
         </div>
       ))}
     </div>
@@ -24,12 +22,3 @@ export const TopicListLoading = () => (
 );
 
 export default TopicListLoading;
-
-export const TopicLoading = () => (
-  <>
-    <TopicListLoading />
-    <TopicListLoading />
-    <TopicListLoading />
-    <TopicListLoading />
-  </>
-);
--- app/loading.tsx
+++ app/loading.tsx
@@ -3,12 +3,12 @@ import TopicListLoading from "./components/loading/components/TopicListLoading";
 
 export default function Loading() {
   return (
-    <div className="grid grid-cols-5 lg:grid-cols-4 gap-3">
-      <section className="col-span-5 lg:col-span-1 p-0 m-0 max-h-max lg:h-full">
+    <div className="grid grid-cols-5 lg:grid-cols-4 gap-10">
+      <section className="col-span-5 lg:col-span-1 p-0 m-0 lg:h-full">
         <TopicListLoading />
       </section>
-      <section className="col-span-5 lg:col-span-3 ">
-        <PostItemCardLoading />
+      <section className="col-span-5 lg:col-span-3">
+        <PostItemCardLoading count={3} />
       </section>
     </div>
   );
