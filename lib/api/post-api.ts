e4b0ef5c98acc34d2e48a35394dceb19eb99e1bc
ghislaingenay <ghislain.gny@gmail.com>
2023-07-29 00:21:49 +0700

    Implement staging + fix issues for new articles

--- lib/api/post-api.ts
+++ lib/api/post-api.ts
@@ -1,4 +1,5 @@
 import { ClipboardCode } from "@app/components/mdx/ClipboardCode";
+import { CounterState } from "@app/components/mdx/CounterState";
 import { CustomImage, Video } from "@components/mdx";
 import { Language, PostTopic } from "@interfaces/global.interface";
 import { SerializeOptions } from "next-mdx-remote/dist/types";
@@ -12,7 +13,7 @@ const HEADERS_GITHUB = {
   "X-GitHub-Api-Version": "2022-11-28",
 };
 
-const COMPONENTS_MDX = { Video, CustomImage, ClipboardCode };
+const COMPONENTS_MDX = { Video, CustomImage, ClipboardCode, CounterState };
 const OPTIONS_MDX: SerializeOptions = {
   parseFrontmatter: true,
   mdxOptions: {
@@ -24,68 +25,82 @@ const OPTIONS_MDX: SerializeOptions = {
   },
 };
 
+export async function getStagingPostByName(
+  fileName: string
+): Promise<Post | undefined> {
+  const res = await fetch(
+    `https://raw.githubusercontent.com/ghislaingenay/blog-posts/master/staging/${fileName}`,
+    {
+      headers: HEADERS_GITHUB,
+    }
+  );
+  if (!res.ok) return undefined;
+  const rawMDX = await res.text();
+  if (/^404[:]/.test(rawMDX)) return undefined;
+  const { frontmatter, content } = await compileMDX<Omit<PostMeta, "id">>({
+    source: rawMDX,
+    components: COMPONENTS_MDX,
+    options: OPTIONS_MDX,
+  });
+  const id = fileName.replace(/\.mdx$/, "");
+  const postObj: Post = { meta: { ...frontmatter, id }, content };
+  return postObj;
+}
+
 export async function getPostByName(
   fileName: string
 ): Promise<Post | undefined> {
-  try {
-    const language = Language.ENGLISH as string;
-    const res = await fetch(
-      `https://raw.githubusercontent.com/ghislaingenay/blog-posts/master/${language}/${fileName}`,
-      {
-        headers: HEADERS_GITHUB,
-      }
-    );
-    if (!res.ok) throw new Error("Failed to fetch post");
-    const rawMDX = await res.text();
-    if (/^404[:]/.test(rawMDX)) throw new Error("Failed to fetch post");
-    const { frontmatter, content } = await compileMDX<Omit<PostMeta, "id">>({
-      source: rawMDX,
-      components: COMPONENTS_MDX,
-      options: OPTIONS_MDX,
-    });
-    const id = fileName.replace(/\.mdx$/, "");
-    const postObj: Post = { meta: { ...frontmatter, id }, content };
-    return postObj;
-  } catch (err: any) {
-    throw new Error(err);
-  }
+  const language = Language.ENGLISH as string;
+  const res = await fetch(
+    `https://raw.githubusercontent.com/ghislaingenay/blog-posts/master/${language}/${fileName}`,
+    {
+      headers: HEADERS_GITHUB,
+    }
+  );
+  if (!res.ok) return undefined;
+  const rawMDX = await res.text();
+  if (/^404[:]/.test(rawMDX)) return undefined;
+  const { frontmatter, content } = await compileMDX<Omit<PostMeta, "id">>({
+    source: rawMDX,
+    components: COMPONENTS_MDX,
+    options: OPTIONS_MDX,
+  });
+  const id = fileName.replace(/\.mdx$/, "");
+  const postObj: Post = { meta: { ...frontmatter, id }, content };
+  return postObj;
 }
 ////////////////////////////////////////////////////////////////////////////
 export async function getPostsMeta(): Promise<PostMeta[] | undefined> {
-  try {
-    const res = await fetch(
-      "https://api.github.com/repos/ghislaingenay/blog-posts/git/trees/master?recursive=1",
-      {
-        headers: HEADERS_GITHUB,
-      }
-    );
-    if (!res.ok) throw new Error("Failed to fetch posts metadata");
-    const repoFileTree: Filetree = await res.json();
-
-    const englishFileTreeUrl = repoFileTree.tree.find(
-      (file) => file.path === "en"
-    )?.url;
-    if (!englishFileTreeUrl) throw new Error("Failed to fetch posts metadata");
-    const englishRes = await fetch(englishFileTreeUrl, {
+  const res = await fetch(
+    "https://api.github.com/repos/ghislaingenay/blog-posts/git/trees/master?recursive=1",
+    {
       headers: HEADERS_GITHUB,
-    });
-    const englishFileTree: Filetree = await englishRes.json();
-    const filesArray = englishFileTree.tree
-      .map((treeObject) => treeObject.path)
-      .filter((path) => path.endsWith(".mdx"));
-
-    const posts: PostMeta[] = [];
-    for (const file of filesArray) {
-      const post = await getPostByName(file);
-      if (post) posts.push(post.meta);
     }
+  );
+  if (!res.ok) throw new Error("Failed to fetch posts metadata");
+  const repoFileTree: Filetree = await res.json();
 
-    return posts.sort((a, b) =>
-      a.createdAt.localeCompare(b.createdAt) ? 1 : -1
-    );
-  } catch (err: any) {
-    throw new Error(err);
+  const englishFileTreeUrl = repoFileTree.tree.find(
+    (file) => file.path === "en"
+  )?.url;
+  if (!englishFileTreeUrl) throw new Error("Failed to fetch posts metadata");
+  const englishRes = await fetch(englishFileTreeUrl, {
+    headers: HEADERS_GITHUB,
+  });
+  const englishFileTree: Filetree = await englishRes.json();
+  const filesArray = englishFileTree.tree
+    .map((treeObject) => treeObject.path)
+    .filter((path) => path.endsWith(".mdx"));
+
+  const posts: PostMeta[] = [];
+  for (const file of filesArray) {
+    const post = await getPostByName(file);
+    if (post) posts.push(post.meta);
   }
+
+  return posts.sort((a, b) =>
+    a.createdAt.localeCompare(b.createdAt) ? 1 : -1
+  );
 }
 /////////////////////////////////////
 export function getPostTopics(posts: PostMeta[]): PostTopicSearch[] {
