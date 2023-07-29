4c6a9390c94b976b3280c267b11b13eafd40febd
ghislaingenay <ghislain.gny@gmail.com>
2023-07-29 17:03:17 +0700

    Remove commented code

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,14 +1,5 @@
 import { PostItemCardLoading } from "./components/loading/components/PostItemLoading";
 
 export default function Loading() {
-  return (
-    // <div className="grid grid-cols-5 lg:grid-cols-4 gap-10">
-    //   <section className="col-span-5 lg:col-span-1 p-0 m-0 lg:h-full">
-    //     <TopicListLoading />
-    //   </section>
-    //   <section className="col-span-5 lg:col-span-3">
-    <PostItemCardLoading count={3} />
-    //   </section>
-    // </div>
-  );
+  return <PostItemCardLoading count={3} />;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -15,10 +15,6 @@ export default async function Home() {
 
   return (
     <>
-      {/* <div className="grid grid-cols-1 gap-3"> */}
-      {/* <section className="col-span-5 lg:col-span-1 p-0 m-0 max-h-max lg:h-full">
-          <TopicList topics={foundTopics} />
-        </section> */}
       <section className="col-span-1">
         <div role="listbox">
           {posts.map((post) => (
@@ -28,7 +24,6 @@ export default async function Home() {
           ))}
         </div>
       </section>
-      {/* </div> */}
       <Searchbar posts={posts} />
     </>
   );
