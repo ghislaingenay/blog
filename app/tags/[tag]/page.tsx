b87c73bb85c70a4c808ec617dfcfd737d71cc3c1
ghislaingenay <ghislain.gny@gmail.com>
2023-07-29 12:42:20 +0700

    Fix error before build

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -12,12 +12,12 @@ export interface TagProps {
 
 export const revalidate = REVALIDATION_PERIOD;
 
-// export async function generateStaticParams() {
-//   const posts = await getPostsMeta(); //deduped!
-//   if (!posts) return [];
-//   const tags = new Set(posts.map((post) => post.tags).flat());
-//   return Array.from(tags).map((tag) => ({ tag }));
-// }
+export async function generateStaticParams() {
+  const posts = await getPostsMeta(); //deduped!
+  if (!posts) return [];
+  const tags = new Set(posts.map((post) => post.tags).flat());
+  return Array.from(tags).map((tag) => ({ tag }));
+}
 
 export function generateMetadata({ params: { tag } }: TagProps) {
   return createMetaData({ title: `Posts about ${tag}` });
