4e678df6749ce29ccbeb66c25084712addf34b3a
ghislaingenay <ghislain.gny@gmail.com>
2023-07-31 09:54:52 +0700

    Fix alignment in postItem

--- app/components/posts/PostItem.tsx
+++ app/components/posts/PostItem.tsx
@@ -25,7 +25,6 @@ export default function PostItem({ post, tag }: PostItemProps) {
   const changeOpacityTagClass = (tagValue: string) =>
     tagMatch(tagValue) ? "" : "opacity-50";
   return (
-    // <li className="border border-red-600">
     <Link
       id={topic}
       href={`/posts/${post.id}`}
@@ -33,7 +32,7 @@ export default function PostItem({ post, tag }: PostItemProps) {
         ...LINK_STYLE,
       }}
     >
-      <div className=" bg-slate-100 shadow-md p-3 max-w-sm min-h-[400px] md:min-h-[160px] md:max-w-full mx-auto md:px-10 grid grid-cols-1 gap-y-4 md:grid-cols-3 md:grid-rows-1 md:gap-x-10 place-self-center">
+      <div className=" bg-slate-100 shadow-md p-3 max-w-sm min-h-[400px] md:min-h-[160px] md:max-w-full lg:max-w-[80%] mx-auto md:px-10 grid grid-cols-1 gap-y-4 md:grid-cols-3 md:grid-rows-1 md:gap-x-10 place-self-center">
         <div className="col-span-1 md:col-start-1 md:col-end-2">
           <Image
             src={image}
@@ -44,7 +43,7 @@ export default function PostItem({ post, tag }: PostItemProps) {
           />
         </div>
         <div className="col-span-1 md:col-start-2 md:col-end-4">
-          <div className="grid grid-cols-1 gap-y-4">
+          <div className="grid grid-cols-1 gap-y-4 lg:h-full">
             <div className="col-span-1">
               <div className="flex justify-between items-center">
                 <TopicTag>{topic}</TopicTag>
@@ -57,7 +56,6 @@ export default function PostItem({ post, tag }: PostItemProps) {
             <h3 className="text-base sm:text-lg line-clamp-2 col-span-1 m-0 truncate">
               {title}
             </h3>
-
             <div className="text-start text-base m-0 col-span-1">
               {tags.map((tag, index) => (
                 <Tag
