4dbd45f976437561bb36554c639e0b5f9af9df58
ghislaingenay <ghislain.gny@gmail.com>
2023-07-31 18:02:54 +0700

    Fix minor issues to get Typescript for dictionary

--- app/[lang]/dictionaries.ts
+++ app/[lang]/dictionaries.ts
@@ -1,8 +1,10 @@
 import { Language } from "@interfaces/global.interface";
 import "server-only";
 
-const dictionaries: Partial<Record<Language, any>> = {
+const dictionaries = {
   [Language.ENGLISH]: () =>
+    import("@public/locales/en/common.json").then((module) => module.default),
+  [Language.FRENCH]: () =>
     import("@public/locales/en/common.json").then((module) => module.default),
 };
 
