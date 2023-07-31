40b48d6efeb89aa4585e397b15e2038cc8f4567b
ghislaingenay <ghislain.gny@gmail.com>
2023-07-31 09:08:44 +0700

    Fix layout for html

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className="min-h-full overflow-x-hidden"
+      className="min-h-full overflow-x-hidden max-w-full"
       // suppressHydrationWarning={true}
     >
       <body className="bg-slate-50 pb-5">
