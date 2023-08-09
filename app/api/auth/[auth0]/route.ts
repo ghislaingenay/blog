b3c1e325056530ffa75d13e0aebbe3031a74415e
ghislaingenay <ghislain.gny@gmail.com>
2023-08-09 10:17:02 +0700

    Re add domain info for dev/prod env in auth0 routes

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,21 +1,21 @@
 import { handleAuth, handleLogout } from "@auth0/nextjs-auth0";
 
-// const domain =
-//   process.env.NODE_ENV === "production"
-//     ? process.env.WEBSITE_URL
-//     : "localhost:3000";
+const domain =
+  process.env.NODE_ENV === "production"
+    ? process.env.WEBSITE_URL
+    : "http://localhost:3000/";
 
 export const GET = handleAuth({
-  //   login: handleLogin({
-  //     authorizationParams: {
-  //       audience: process.env.AUTH0_AUDIENCE,
-  //       redirect_uri: `https://${domain}/api/auth/callback`,
-  //     },
-  //   }),
-  //   callback: handleCallback({
-  //     redirectUri: `https://${domain}/api/auth/callback`,
-  //   }),
+  // login: handleLogin({
+  //   authorizationParams: {
+  //     audience: process.env.AUTH0_AUDIENCE,
+  //     redirect_uri: `${domain}/api/auth/callback`,
+  //   },
+  // }),
+  // callback: handleCallback({
+  //   redirectUri: `${domain}/api/auth/callback`,
+  // }),
   logout: handleLogout({
-    returnTo: `http://localhost:3000/`,
+    returnTo: domain,
   }),
 });
