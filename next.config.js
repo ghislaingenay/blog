ba7b6213acb71e7b61721f1992522bbccc5e55aa
ghislaingenay <ghislain.gny@gmail.com>
2023-08-11 11:52:22 +0700

    Fix import fs issues in nextjs

--- next.config.js
+++ next.config.js
@@ -32,6 +32,13 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+
+    return config;
+  },
 };
 
 module.exports = withMDX(nextConfig);
