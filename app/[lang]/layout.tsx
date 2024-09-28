a8b79f7bb506ba192bac3d38b77622bf45500a8a
ghislaingenay <ghislain.gny@gmail.com>
2024-09-28 12:57:15 +0700

    chore: remove nav in homepage
    Redo everything from scratch from now

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,11 +1,7 @@
 import { createMetaData } from "@functions";
 import { LangProps, Language, LayoutProps } from "@interfaces";
-import { Inter } from "next/font/google";
-import Navbar from "../components/navigation/Navbar";
 import "./../globals.css";
 import { getDictionary } from "./dictionaries";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export async function generateMetadata({ params: { lang } }: LangProps) {
   const dict = await getDictionary(lang);
@@ -28,7 +24,7 @@ export default async function RootLayout({
   return (
     <html lang={dict.language} className="min-h-full overflow-x-hidden">
       <body className="bg-slate-50 pb-5 bg-gradient-to-b from-zinc-100 to-zinc-50">
-        <Navbar dict={dict} />
+        {/* <Navbar dict={dict} /> */}
         <main className="container mx-auto sm:w-[600px] md:w-[728px] lg:w-[984px] xl:w-[1240px] 2xl:[1535px] px-5 sm:px-0 prose prose-slate prose-h1:m-0 ">
           {children}
         </main>
