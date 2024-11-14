3dc8c14aa63fc7c88eec00c7502ae36b81f78477
ghislaingenay <ghislain.gny@gmail.com>
2024-11-14 13:55:35 +0700

    refactor: Simplify HeroBanner layout and update tag display

--- app/components/routes/homepage/HeroBanner.tsx
+++ app/components/routes/homepage/HeroBanner.tsx
@@ -1,4 +1,3 @@
-import Grid from "@components/Grid";
 import { Tag } from "@components/Tag";
 import MyPicture from "@public/ghislain.jpg";
 import Image from "next/image";
@@ -6,66 +5,62 @@ import Image from "next/image";
 export default function HeroBanner() {
   return (
     <section className="flex gap-4">
-      <span className="flex-1 hidden md:block">
+      <span className="hidden md:block">
         <Image
           src={MyPicture}
           width={200}
           height={200}
-          className="rounded-full w-full"
+          className="rounded-full w-[300px]"
           alt={`Ghislain Genay's profile picture`}
         />
       </span>
-      <div className="flex-1 grid">
+      <div className="flex-grow">
         <h1>Ghislain Genay</h1>
         <h2>Software Engineer</h2>
         <p>
           Full Stack Engineer specializing in scalable web applications, ready
           to transform your ideas into innovative solutions.
         </p>
-        <Grid>
-          <span className="flex gap-2">
-            <Tag className="h-[40px]" color="blue">
-              React
-            </Tag>
-            <Tag className="h-[40px]" color="blue">
-              React
-            </Tag>
-            <Tag className="h-[40px]" color="blue">
-              React
-            </Tag>
-            <Tag className="h-[40px]" color="blue">
-              React
-            </Tag>
-          </span>
-          <span className="flex gap-2">
-            <Tag className="h-[40px]" color="blue">
-              React
-            </Tag>
-            <Tag className="h-[40px]" color="blue">
-              React
-            </Tag>
-            <Tag className="h-[40px]" color="blue">
-              React
-            </Tag>
-            <Tag className="h-[40px]" color="blue">
-              React
-            </Tag>
-          </span>
-          <span className="flex gap-2">
-            <Tag className="h-[40px]" color="blue">
-              React
-            </Tag>
-            <Tag className="h-[40px]" color="blue">
-              React
-            </Tag>
-            <Tag className="h-[40px]" color="blue">
-              React
-            </Tag>
-            <Tag className="h-[40px]" color="blue">
-              React
-            </Tag>
-          </span>
-        </Grid>
+
+        <div className="flex gap-2 flex-wrap">
+          <Tag className="h-[40px]" color="blue">
+            React
+          </Tag>
+          <Tag className="h-[40px]" color="blue">
+            React
+          </Tag>
+          <Tag className="h-[40px]" color="blue">
+            React
+          </Tag>
+          <Tag className="h-[40px]" color="blue">
+            React
+          </Tag>
+          <Tag className="h-[40px]" color="blue">
+            React
+          </Tag>
+          <Tag className="h-[40px]" color="blue">
+            React
+          </Tag>
+          <Tag className="h-[40px]" color="blue">
+            React
+          </Tag>
+          <Tag className="h-[40px]" color="blue">
+            React
+          </Tag>
+
+          <Tag className="h-[40px]" color="blue">
+            React
+          </Tag>
+          <Tag className="h-[40px]" color="blue">
+            React
+          </Tag>
+          <Tag className="h-[40px]" color="blue">
+            React
+          </Tag>
+          <Tag className="h-[40px]" color="blue">
+            React
+          </Tag>
+        </div>
       </div>
     </section>
   );
